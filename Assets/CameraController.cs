a8bde67e69fa7bf034fee234c2c3fee9ff176801
Matt D <autoknow.matt.d@gmail.com>
2023-11-23 11:52:13 +1030

    Added camera y rotation with mouse movement

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -6,17 +6,32 @@ public class CameraController : MonoBehaviour
 {
     public GameObject ball;
     public Vector3 offset;
-
+    public float cameraRotationDegrees = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = ball.transform.position + offset;
+        if(Input.GetAxis("Mouse X") != 0)
+        {
+            cameraRotationDegrees += Input.GetAxis("Mouse X");
+        }
+
+        //Set the camera rotation to our stored rotation value
+        transform.rotation = Quaternion.Euler(0, cameraRotationDegrees, 0);
+
+        //Position the camera directly onto the ball
+        transform.position = ball.transform.position;
+
+        //Move the camera backwards
+        transform.position -= transform.forward * offset.y;
+
+        //Move the camera up
+        transform.position += Vector3.up * offset.z;
     }
 }
