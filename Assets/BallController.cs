472d2e95c5806289fc748e5a3516cc60bd09b1b2
Liam.N <liamgamer992@gmail.com>
2023-11-23 11:15:41 +1100

    init commitment

---
+++ Assets/BallController.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    public Rigidbody rigidBody;
+    public float jumpForce;
+    public int jumpCount;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    if (Input.GetKey(KeyCode.W))
+        {
+            rigidBody.AddForce(Vector3.forward);
+        }
+    if (Input.GetKey(KeyCode.S))
+        {
+            rigidBody.AddForce(Vector3.back);
+        }
+     if (Input.GetKey(KeyCode.D))
+            {
+                rigidBody.AddForce(Vector3.right);
+
+            }
+        
+           
+        if (Input.GetKey(KeyCode.A)) { rigidBody.AddForce(Vector3.left); }
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            if (rigidBody.velocity.y == 0)
+            {
+                rigidBody.AddForce(Vector3.up * jumpForce);
+                jumpCount = 1;
+
+            }
+            else
+            {
+                if (jumpCount == 1)
+                {
+                     rigidBody.velocity = new Vector3(rigidBody.velocity.x, 0, rigidBody.velocity.z);
+                    rigidBody.AddForce(Vector3.up * jumpForce);
+                    jumpCount = 2;
+                }
+            }
+            
+        }
+       
+            
+    }
+
+}
