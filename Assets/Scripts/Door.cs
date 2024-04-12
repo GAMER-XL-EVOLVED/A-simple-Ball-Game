6fc2973bfbe99282bf26a76ecfad20bbbd6c967e
GAMER-XL-EVOLVED <151795997+GAMER-XL-EVOLVED@users.noreply.github.com>
2024-04-12 10:38:45 +1000

    Added multiple additions
    Made a door, made a staircase, animated  the door and added a teleporter

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public Animator myAnimator;
+    public int coinsRequired;
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/HUD.cs
+++ Assets/Scripts/HUD.cs
@@ -7,9 +7,13 @@ public class HUD : MonoBehaviour
 {
     public BallController myBallController;
     public Text coinText;
-
+    public Door myDoor;
     private void Update()
     {
         coinText.text = myBallController.coinsCollected.ToString();
+        if (myDoor.coinsRequired <= myBallController.coinsCollected)
+        {
+            myDoor.myAnimator.SetTrigger("Open");
+        }
     }
 }
