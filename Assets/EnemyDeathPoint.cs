327ae09d3a67526465dd3cdb1c6f333e90fd58be
GAMER-XL-EVOLVED <151795997+GAMER-XL-EVOLVED@users.noreply.github.com>
2024-08-16 11:13:17 +1000

    Tried to make the enemy killable

---
+++ Assets/EnemyDeathPoint.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDeathPoint : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("The enemy should be deleted");
+        if (other.tag == "Player")
+        {
+            Destroy(this);
+
+
+        }
+        
+    }
+}
